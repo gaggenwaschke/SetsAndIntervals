2edce710345f2a4ae09af928f1151fe19e7ba75c
Gaggenwaschke <gaggen@hotmail.de>
2022-12-14 01:17:11 +0100

    Let math::operations return auto and use true_type and false_type

--- src/public/math/operations.hpp
+++ src/public/math/operations.hpp
@@ -1,21 +1,23 @@
 
 #pragma once
 
+#include <type_traits>
+
 namespace math::operations {
 struct conjunction_type {
-  constexpr bool operator()(const auto &...args) { return (args && ...); };
-  constexpr static bool neutral_element{true};
+  constexpr auto operator()(const auto &...args) { return (args && ...); };
+  constexpr static std::true_type neutral_element{};
 };
 constexpr conjunction_type conjunction{};
 
 struct disjunction_type {
-  constexpr bool operator()(const auto &...args) { return (args || ...); };
-  constexpr static bool neutral_element{false};
+  constexpr auto operator()(const auto &...args) { return (args || ...); };
+  constexpr static std::false_type neutral_element{};
 };
 constexpr disjunction_type disjunction{};
 
 struct symmetric_difference_type {
-  constexpr bool operator()(const auto &...args) { return (args ^ ...); }
+  constexpr auto operator()(const auto &...args) { return (args ^ ...); }
 };
 constexpr symmetric_difference_type symmetric_difference{};
 } // namespace math::operations
